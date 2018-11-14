58540983b017fa0bc0a74e93a59b990ce186c109
Dorababu Gangapatnam -X (dgangapa - COGNIZANT TECHNOLOGY SOLUTIONS US CORPORATION at Cisco) <dgangapa@partnet.cisco.com>
2018-11-14 10:31:55 +0530

    test

--- ScalaWrapper/src/main/java/ScalaWrapper.scala
+++ ScalaWrapper/src/main/java/ScalaWrapper.scala
@@ -26,8 +26,12 @@ object ScalaWrapper {
     val job_steps=scala.io.Source.fromFile(args(0)).getLines().filter(!_.isEmpty())
     .map(line=>line.split(",").map(_.trim())).toArray
     
+    def hql_query(loc:String)={
+      scala.io.Source.fromFile(loc).mkString
+    }
+    
     for(i <- job_steps.indices){
-      createTable(job_steps(i)(1),job_steps(i)(2),job_steps(i)(3),job_steps(i)(4),job_steps(i)(5))
+      createTable(job_steps(i)(1),job_steps(i)(2),job_steps(i)(3),job_steps(i)(4),hql_query(job_steps(i)(5)))
     }
     def createTable(TempTableReq:String,BroadCast:String,Cached:String,TempTableName:String,hql_text:String){
       if(TempTableReq=="Y"){
