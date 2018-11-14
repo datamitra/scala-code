bf90f6ecbcf7b9fadd1ff8883ff4b92b6cc59fb8
Dorababu Gangapatnam -X (dgangapa - COGNIZANT TECHNOLOGY SOLUTIONS US CORPORATION at Cisco) <dgangapa@partnet.cisco.com>
2018-11-14 10:06:10 +0530

    test

---
+++ ScalaWrapper/src/main/java/ScalaWrapper.scala
@@ -0,0 +1,57 @@
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+
+object ScalaWrapper {
+  def main(args: Array[String]): Unit = {
+    val logger = Logger.getRootLogger
+    val conf=new SparkConf()
+    val spark=SparkSession.builder()
+    .config(conf)
+    .config("hive.exec.dynamic.partition.mode","nonstrict")
+    .enableHiveSupport()
+    .getOrCreate()
+    
+    val sc=spark.sparkContext
+    
+    logger.info("Application ID is :"+sc.applicationId)
+    logger.warn("Application ID is :"+sc.applicationId)
+    
+    //Read CSV file
+    //stepno,temptable?, broadcast?,cache?,temptablename,sqlfile
+    //1,Y,Y,Y,"xyz",step1.hql   select
+    //2,N,N,N,,step2.hql  insert into
+    logger.info("Reading from file :"+args(0))
+    val job_steps=scala.io.Source.fromFile(args(0)).getLines().filter(!_.isEmpty())
+    .map(line=>line.split(",").map(_.trim())).toArray
+    
+    for(i <- job_steps.indices){
+      createTable(job_steps(i)(1),job_steps(i)(2),job_steps(i)(3),job_steps(i)(4),job_steps(i)(5))
+    }
+    def createTable(TempTableReq:String,BroadCast:String,Cached:String,TempTableName:String,hql_text:String){
+      if(TempTableReq=="Y"){
+            if(Cached=="Y"){
+              logger.info("Temp Table name is:"+TempTableName)
+              spark.sql(hql_text).cache().createOrReplaceTempView(TempTableName)
+            }
+            else{
+              spark.sql(hql_text).createOrReplaceTempView(TempTableName)
+            }
+          //Still temp table required
+        if(BroadCast=="Y"){
+          sc.broadcast(TempTableName)
+        }
+        else{ // temp table not required
+          spark.sql(hql_text)
+          logger.info("*********** Job Succeeded*********")
+
+        }
+    }    
+
+  }
+    sc.stop()
+    spark.stop()
+} // main method end
+  
+}//Object end
